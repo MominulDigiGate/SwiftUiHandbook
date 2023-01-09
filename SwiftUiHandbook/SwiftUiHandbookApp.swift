c18034187b43ff28f7d0eace6c7c435ec166bdbf
Mominul <mominul.islam@digigate30.com>
2023-01-10 00:56:24 +0600

    Initial Commit

---
+++ SwiftUiHandbook/SwiftUiHandbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiHandbookApp.swift
+//  SwiftUiHandbook
+//
+//  Created by Mac Mini 5 on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiHandbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
